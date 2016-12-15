70f2b6325557e3980a219c00ff76c1cc46df4e3b
yangenneng <1274491995@qq.com>
2016-12-15 20:09:32 +0800

    项目修复完毕，解决办法：clear之前的工程->Rebuild project

--- app/src/main/java/com/example/yangenneng0/myapplication/db/ConnectionFactory.java
+++ app/src/main/java/com/example/yangenneng0/myapplication/db/ConnectionFactory.java
@@ -8,12 +8,13 @@ import java.sql.SQLException;
  * User: yangenneng
  * DateTime: 2016/12/15 18:38
  * Description:数据库连接工厂类
+ * 单例模式
  */
 public class ConnectionFactory {
     private static final String DB_DRIVER="com.mysql.jdbc.Driver";      //数据库驱动
     private static final String DB_URL = "jdbc:mysql://119.29.89.224:3306/qqdb?useUnicode=true&characterEncoding=UTF-8";//连接数据库的URL地址
     private static final String DB_USERNAME = "root";                   //数据库的用户名
-    private static final String DB_PASSWORD ="admin";                   //数据库的密码
+    private static final String DB_PASSWORD ="";                   //数据库的密码
 
     public static Connection getConnection() {
         Connection conn = null;
